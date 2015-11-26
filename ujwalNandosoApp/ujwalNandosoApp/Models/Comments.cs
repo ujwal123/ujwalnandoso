d20e788a5c4e365d67ec5114636256f1d43f5574
ujwal123 <ujwal800@yahoo.co.nz>
2015-11-26 22:36:52 +1300

    v5: added signalR

---
+++ ujwalNandosoApp/ujwalNandosoApp/Models/Comments.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ujwalNandosoApp.Models
+{
+    public class Comments
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int commentID { get; set; }
+
+        public string name { get; set; }
+
+        public string comment { get; set; }
+
+        public virtual ICollection<string> commentsPosted { get; set; }
+
+
+    }
+}
---
+++ ujwalNandosoApp/ujwalNandosoApp/Models/ujwalNandosoAppContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ujwalNandosoApp.Models
+{
+    public class ujwalNandosoAppContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public ujwalNandosoAppContext() : base("name=ujwalNandosoAppContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<ujwalNandosoApp.Models.Comments> Comments { get; set; }
+    }
+}
